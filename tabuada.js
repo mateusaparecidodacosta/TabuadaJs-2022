fc4a61ce66e3774685c28df10aee37bf5829f9fc
Mateus <mateusadc19@gmail.com>
2022-11-24 20:21:59 -0300

    Finalizar Tarefa

---
+++ tabuada.js
@@ -0,0 +1,15 @@
+const numeroDigitado = prompt("Digite um Número");
+let n2 = 0;
+let contador =1;
+let resultado = 'tabuada do número escolhido:';
+
+while(n2 < 10)
+
+
+{
+    n2 = n2 + 1;
+    contador = numeroDigitado * n2;
+    resultado = `${resultado}\n${numeroDigitado} x ${n2} = ${contador}`;
+}
+
+alert(resultado);
